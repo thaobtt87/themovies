a1b50b231355eaada131084793cf7dd1f1fc2881
thaobtt87 <thaobtt87@gmail.com>
2018-04-23 08:08:25 +0700

    minor improvements

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,7 +7,7 @@ import { MovieDetailComponent } from './movie-detail/movie-detail.component';
 import { NotfoundComponent } from './notfound/notfound.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: '/about', pathMatch: 'full' },
+  { path: '', redirectTo: '/movies', pathMatch: 'full' },
   { path: 'movies', component: MoviesComponent},
   { path: 'movie/:id', component: MovieDetailComponent},
   { path: 'about', component: AboutComponent},
